4df7ed11b16e652de91363dad100840060049bf8
adrien meinier <adrien.meinier@laplateforme.io>
2026-03-30 12:05:54 +0200

    lets start

---
+++ exos/PUB.java
@@ -0,0 +1,13 @@
+public class PUB {
+  
+    public static void main(String[] args) {
+      
+        int c = 1;
+
+      // Using do-while loop
+        do {
+            System.out.println("DES PUBS: " + c);
+            c++;
+        } while (c <= 5);
+    }
+}
