7893032fe0b80a27035cb3ae9ec70a9bb25002f9
adrien meinier <adrien.meinier@laplateforme.io>
2026-03-30 13:30:40 +0200

    job01

--- jour01/job01/Main.java
+++ jour01/job01/Main.java
@@ -1,6 +1,20 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello madafaka");
+    public static void main(String[] args) {
+        
+        // Variable declarations
+        char letter = 'A';
+        String text = "Hello";
+        int integerNumber = 10;
+        long largeNumber = 100000L;
+        float decimalNumber = 3.14f;
+        boolean isTrue = true;
+
+        // Display values
+        System.out.println("char: " + letter);
+        System.out.println("String: " + text);
+        System.out.println("int: " + integerNumber);
+        System.out.println("long: " + largeNumber);
+        System.out.println("float: " + decimalNumber);
+        System.out.println("boolean: " + isTrue);
     }
-    
 }
