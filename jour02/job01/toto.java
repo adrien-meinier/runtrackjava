23d67f1d3cc1b5c945a1a87051300653a6d1ac67
adrien meinier <adrien.meinier@laplateforme.io>
2026-03-31 10:30:14 +0200

    leroy toto

---
+++ jour02/job01/toto.java
@@ -0,0 +1,13 @@
+/*class Toto {
+    int toto = 0;
+    Toto() {
+        toto = toto +1 ;
+    }
+    public static void main(String[]tutu) {
+        Toto t1 = new Toto();
+        Toto t2 = new Toto();
+        System.out.println("Toto :" + toto);
+    }
+
+}*/
+// non ne fonctionne pas probleme du au static sacre toto
