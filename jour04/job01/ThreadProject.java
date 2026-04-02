89d0a121e766357c8106ef5b03f1061e5f37c45c
adrien meinier <adrien.meinier@laplateforme.io>
2026-04-02 11:20:10 +0200

    TheThread

---
+++ jour04/job01/ThreadProject.java
@@ -0,0 +1,10 @@
+public class ThreadProject {
+
+    public static void main(String[] args) {
+        // Instanciation du thread
+        MyThread thread = new MyThread();
+
+        // Lancement du thread
+        thread.start();
+    }
+}
